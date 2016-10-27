ad0b777a61717018f3774d6d53f0ffc4d06c5efa
William Mayo <wlmayo@gmail.com>
2016-10-27 14:40:43 -0400

    attempting a search feature but getting errors with search term and get method

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,16 @@
 // why is this router?
 const router = require('express').Router();
 const { getExchangeRate } = require('../services/fixerApi');
+const { baseRate } = require('../services/fixerApi');
 
 router.get('/', getExchangeRate, (req, res) => {
+  console.log(res.money)
+  res.render('index', {
+    money: res.money
+  })
+})
+
+router.post('/search', baseRate, (req, res) => {
   console.log(res.money)
   res.render('index', {
     money: res.money
--- services/fixerApi.js
+++ services/fixerApi.js
@@ -1,6 +1,19 @@
 const fetch = require('node-fetch');
 
-const API_URL = 'http://api.fixer.io/latest';
+const API_URL_SEARCH = 'http://api.fixer.io/latest?';
+
+function baseRate(req, res, next) {
+  fetch(`${API_URL_SEARCH}base=$req.query.searchTerm}`)
+  .then(r => r.json())
+  .then(result => {
+    res.money = result; // res.money = result.rates will bring just the rates
+    next();
+  }).catch(err => {
+    next();
+  });
+}
+
+const API_URL = 'http://api.fixer.io/latest?base=USD';
 
 function getExchangeRate(req, res, next) {
   fetch(API_URL)
@@ -13,4 +26,4 @@ function getExchangeRate(req, res, next) {
   });
 }
 
-module.exports = { getExchangeRate };
+module.exports = { baseRate, getExchangeRate };
