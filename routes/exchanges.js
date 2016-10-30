0a73815da5e84a078785f3a6bde70d0a6aa56c1c
William Mayo <wlmayo@gmail.com>
2016-10-30 12:45:05 -0400

    saving favorites to mongo for users is functional but not rendering to page

--- routes/exchanges.js
+++ routes/exchanges.js
@@ -3,13 +3,16 @@ const { authenticate }    = require('../lib/auth');
 const { baseRate }     = require('../services/fixerApi');
 const { getExchangeRate } = require('../services/fixerApi');
 const { findCountries } = require('../services/restCountriesApi');
+const { saveFavorite } = require('../models/favorites');
+const { getFavorites } = require('../models/favorites');
 
 
-exchangesRouter.get('/', authenticate, (req, res) => {
+exchangesRouter.get('/', authenticate, getFavorites, (req, res) => {
   res.render('exchanges/index', {
     user: res.user || [],
     money: res.money || [],
-    countries: res.countries || []
+    countries: res.countries || [],
+    saved: res.favorites || []
   });
 });
 
@@ -19,8 +22,14 @@ exchangesRouter.get('/search', authenticate, findCountries, baseRate, (req, res)
   res.render('exchanges/index', {
     money: res.money || [],
     user: res.user || [],
-    countries: res.countries || []
+    countries: res.countries || [],
+    saved: res.saved || []
   });
 });
+
+exchangesRouter.post('/exchanges/favorites', authenticate, saveFavorite, (req, res) => {
+  console.log(res.saved);
+  res.redirect('/exchanges');
+})
 
 module.exports = exchangesRouter;
